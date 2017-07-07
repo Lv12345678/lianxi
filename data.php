3a9f2788998fa59be51e5fa6fd83614a9150e874
lv <1149959814@qq.com>
2017-07-07 11:04:58 +0800

    第一次提交

---
+++ data.php
@@ -0,0 +1,10 @@
+<?php
+//模拟一个数据查看请求是否成功
+// echo "abc";
+	// 获取json文件的数据
+	// echo file_get_contents("student.json");
+	// echo $_GET["name"];
+	// 获取POST请求所传递的参数
+	echo $_POST["name"].$_POST["age"];
+	
+?>
